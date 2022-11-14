4cb9ebf31e9629eb20d97aed1899b60e6ea6e04d
Nandhiniganeson <116422612+Nandhiniganeson@users.noreply.github.com>
2022-11-14 15:48:37 +0530

    Delete TempHumiditydpka.py

--- assignments/Aarthi.A/TempHumiditydpka.py
+++
@@ -1,7 +0,0 @@
-import random
-temp_sensor_value=random.randint(21,51)
-humidity_value=random.randint(20,46)
-if temp_sensor_value>30 or humidity_value>90:
-    print("Temperature value:",temp_sensor_value)
-    print("Humidity value:",humidity_value)
-    print("Hazard detected")
